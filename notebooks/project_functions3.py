6162c8a38871367fb0a44242257fdc24dc893372
krehill <rehill.komal@gmail.com>
2021-11-01 15:02:21 -0700

    Completed Tasks 1-4 for Milestone 3

--- notebooks/project_functions3.py
+++ notebooks/project_functions3.py
@@ -3,21 +3,32 @@ import pandas as pd
 
 def load_and_process(pathname):
 
-    # Method Chain 1 (Load data and deal with missing data)
+    # Method Chain 1 (Load data, remove unneeded columns and remove rows with missing data)
 
     ldr1 = (
-          pd.read_csv("../data/raw/EMLSdata_10Aug.csv")
-          .drop(labels=['Date','LabName','Latitude','Longitude','Altitude_m','SurfaceTemperature_C','EpilimneticTemperature_C','CYN_ugL','ATX_ugL','Lutein_ugL','Echinenone_ugL','Chlorophyllc2_ugL','Peridinin_ugL','Alloxanthin_ugL','Diadinoxanthin_ugL','Zeaxanthin_ugL','Chlorophyllb_ugL','Chlorophylla_ugL','SecchiDepth_m'], axis=1)
-          .dropna(axis=0)
-          # etc...
-      )
-
-    # Method Chain 2 (Create new columns, drop others, and do processing)
-
+        pd.read_csv("../data/raw/EMLSdata_10Aug.csv")
+        .drop(labels=['MeanDepth_m','Lake_ID','MC_LY_ugL','MC_LW_ugL','MC_LF_ugL','NOD_ugL','MaximumDepth_m', 'TN_mgL', 'NO3NO2_mgL', 'NH3_mgL', 'PO4_ugL','Date','LabName','Latitude','ThermoclineDepth_m','SamplingDepth_m','Longitude','Altitude_m','SurfaceTemperature_C','EpilimneticTemperature_C','CYN_ugL','ATX_ugL','Lutein_ugL','Echinenone_ugL','Chlorophyllc2_ugL','Peridinin_ugL','Alloxanthin_ugL','Diadinoxanthin_ugL','Fucoxanthin_ugL','Zeaxanthin_ugL','Chlorophyllb_ugL','Diatoxanthin_ugL','Chlorophylla_ugL','SecchiDepth_m','Violaxanthin_ugL'], axis=1)
+        .dropna(axis=0)
+    )
+    
+    # Method Chain 2 (Drop unneeded columns, rename columns, drop outlier row, sort with ascending Mean Depth (m)
+    
     ldr2 = (
-          ldr1
-          .rename(columns={"TP_mgl": "Total phosphorus concentration", "TN_mgl": "Total nitrogen concentration", "NO3NO2_mgL": "Nitrates and nitrites concentration", "NH4_mgL": "ammonia concentration", "PO4_mgL": "ortho-phosphate concentration", "MC_YR.ugL": "Concentration of cyanobacterial hepatotoxin microcystin YR", "MC_dmRR.ugL": "Concentration of cyanobacterial hepatotoxin microcystin dmRR", "MC_RR_ugL": "Concentration of cyanobacterial hepatotoxin microcystin RR", "MC_dmLR_ugL": "Concentration of cyanobacterial hepatotoxin microcystin dmLR", "MC_LR_ugL": "Concentration of cyanobacterial hepatotoxin microcystin LR", "MC_LY_ugL": "Concentration of cyanobacterial hepatotoxin microcystin LY", "MC_LW_ugL": "Concentration of cyanobacterial hepatotoxin microcystin LW", "MC_LF_ugL": "Concentration of cyanobacterial hepatotoxin microcystin LF", "NOD_ugL": "Concentration of cyanobacterial hepatotoxin nodularin"})
-          .sort_values(by=['MeanDepth_m'], ascending=True)
-      )
+        ldr1
+        .rename(columns=
+            {"LakeName": "Lake Name", 
+            "TP_mgL": "Concentration of total phosphorus (mgL)",
+            "MC_YR_ugL": "Concentration of cyanobacterial hepatotoxin microcystin YR (ugL)", 
+            "MC_dmRR_ugL": "Concentration of cyanobacterial hepatotoxin microcystin dmRR (ugL)", 
+            "MC_RR_ugL": "Concentration of cyanobacterial hepatotoxin microcystin RR (ugL)", 
+            "MC_dmLR_ugL": "Concentration of cyanobacterial hepatotoxin microcystin dmLR (ugL)", 
+            "MC_LR_ugL": "Concentration of cyanobacterial hepatotoxin microcystin LR (ugL)", 
+            "MC_LW_ugL": "Concentration of cyanobacterial hepatotoxin microcystin LW (ugL)", 
+            "MC_LF_ugL": "Concentration of cyanobacterial hepatotoxin microcystin LF (ugL)", 
+            "NOD_ugL": "Concentration of cyanobacterial hepatotoxin nodularin (ugL)"})
+        .sort_values(by=['Concentration of total phosphorus (mgL)'], ascending=True)
+    )
+    
+    return ldr2
 
-    return ldr2 
+load_and_process('../data/raw/EMLSdata_10Aug.csv')
